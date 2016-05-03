4befe8223842f1d48f0e4e5cdb865e5684454d59
AI Matsubara <aiai.1116.info@gmail.com>
2016-05-03 15:57:36 +0800

    initial commit

--- sampleSwipeRecognizer/ViewController.swift
+++ sampleSwipeRecognizer/ViewController.swift
@@ -14,7 +14,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    
+    //スワイプビュー
+    @IBAction func swipeView(sender: UISwipeGestureRecognizer) {
+        
+        print("スワイプしました")
+    }
+    
+    //TODO:色付きのビューを追加して、そのビューの中で上スワイプを感知し、「上スワイプされました」と表示しましょう
+    
+    @IBAction func swipColorView(sender: UISwipeGestureRecognizer) {
+        
+        print("上スワイプされました")
+    }
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
